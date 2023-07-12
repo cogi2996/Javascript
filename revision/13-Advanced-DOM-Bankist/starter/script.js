7315a827406d7e2349f663d948eb3bdf1200c1ba
cogi2996 <bidang.tuyloan@gmail.com>
2023-07-12 22:22:18 +0700

    'add'

--- revision/13-Advanced-DOM-Bankist/starter/script.js
+++ revision/13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,34 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class ="btn btn--close-cookie"> Got it!</button>';
+header.prepend(message);
+// header.append(message.cloneNode(true));
+header.append(message);
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    // message.parentElement.removeChild(message);
+
+  });
