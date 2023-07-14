1391c2ac2db8f228b79e1610fab9444fce2a3e1b
cogi2996 <bidang.tuyloan@gmail.com>
2023-07-14 12:36:22 +0700

    'add'

--- revision/13-Advanced-DOM-Bankist/starter/script.js
+++ revision/13-Advanced-DOM-Bankist/starter/script.js
@@ -30,18 +30,18 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
 const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+// console.log(allSections);
 
 const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+// console.log(allButtons);
 
-console.log(document.getElementsByClassName('btn'));
+// console.log(document.getElementsByClassName('btn'));
 
 // Creating and inserting elements
 const message = document.createElement('div');
@@ -60,48 +60,67 @@ document
     // message.parentElement.removeChild(message);
   });
 
+//----------------------------------------------------------------
 // Styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
-console.log(message.style.color);
-console.log(message.style.height);
-console.log(getComputedStyle(message).height);
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+// console.log(message.style.color);
+// console.log(message.style.height);
+// console.log(getComputedStyle(message).height);
 
 
-message.style.height =
-  parseInt(getComputedStyle(message).height,10) + 40 + 'px';
-// message.style.setProperty('background-color','red')
+// message.style.height =
+//   parseInt(getComputedStyle(message).height,10) + 40 + 'px';
+// // message.style.setProperty('background-color','red')
 
-document.documentElement.style.setProperty('--color-primary','orangered')
+// document.documentElement.style.setProperty('--color-primary','orangered')
 
-// Attributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.className);
-logo.alt = 'Beautiful minimalist logo';
-// logo.setAttribute('alt', 'Beautiful minimalist logo');
-// Non-standard
-console.log(logo.designer);
-console.log(logo.getAttribute('designer'));
-console.log('-================================================');
+// // Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.className);
+// logo.alt = 'Beautiful minimalist logo';
+// // logo.setAttribute('alt', 'Beautiful minimalist logo');
+// // Non-standard
+// console.log(logo.designer);
+// console.log(logo.getAttribute('designer'));
+// console.log('-================================================');
 
-const link = document.querySelector('.nav__link--btn');
-console.log(link.href);
-console.log(link.getAttribute('href'));
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
 
 
-console.log(link.dataset.versionNumber);
+// console.log(link.dataset.versionNumber);
 
-// Classes
-logo.classList.add('c','j');
-logo.classList.remove('c','j');
-// logo.classList.toggle('c');
-logo.classList.contains('c');
+// // Classes
+// logo.classList.add('c','j');
+// logo.classList.remove('c','j');
+// // logo.classList.toggle('c');
+// logo.classList.contains('c');
 
-// overlay class --> Don't use
-/* logo.className = 'jonas'
-console.log(logo.className); */
+// // overlay class --> Don't use
+// /* logo.className = 'jonas'
+// console.log(logo.className); */
 
-logo.setAttribute('hihi','hoho')
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1')
+
+btnScrollTo.addEventListener('click', function(e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log('Current scrolll (X/Y)', window.pageXOffset, window.pageYOffset);
+  console.log('height/width viewport', document.documentElement.clientWidth,document.documentElement.clientHeight);
+  // Scrolling 
+  // window.scrollTo(s1coords.left+window.pageXOffset, s1coords.top+window.pageYOffset)
+  /* window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior:'smooth'
+  }) */
+  section1.scrollIntoView({behavior:'smooth'});
+})
 
 
