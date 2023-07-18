e2813be65ebb0f118bc5c0a2f6320f506b93b1a9
cogi2996 <bidang.tuyloan@gmail.com>
2023-07-18 23:51:08 +0700

    'add'

--- revision/13-Advanced-DOM-Bankist/starter/script.js
+++ revision/13-Advanced-DOM-Bankist/starter/script.js
@@ -182,19 +182,50 @@ const headerObserve = new IntersectionObserver(stickyNav, {
 });
 headerObserve.observe(header); */
 const navHeight = nav.getBoundingClientRect().height;
-const stickyNav = function([entries]) {
-  console.log(entries);
-  if(!entries.isIntersecting){
+const stickyNav = function ([entries]) {
+  // console.log(entries);
+  if (!entries.isIntersecting) {
     nav.classList.add('sticky');
-  }
-  else{
+  } else {
     nav.classList.remove('sticky');
   }
-    
-}
-const headerObserve =  new IntersectionObserver(stickyNav, {root: null,threshold: 0,rootMargin:`-${navHeight}px`});
+};
+const headerObserve = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
 headerObserve.observe(header);
 
+// Reveal Seciton
+/* const revealSection = function (entries, observer) {
+  const [entry]  = entries;
+  console.log(entry);
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden')
+  observer.unobserve(entry.target)
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root:null,
+  threshold:0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+}); */
+
+const revealSection = function(entries,observer){
+  const [entry] = entries;
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(revealSection,{root:null,threshold:0.15});
+allSections.forEach(function(section){
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
 // Lazy loading images
 /* const imgTarget = document.querySelectorAll('img[data-src]');
 console.log(imgTarget);
