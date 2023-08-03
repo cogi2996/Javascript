83d80947375d1f23d969c44402986b876d570ec3
cogi2996 <bidang.tuyloan@gmail.com>
2023-08-03 22:54:51 +0700

    'add'

--- revision/15-Mapty/starter/script.js
+++ revision/15-Mapty/starter/script.js
@@ -14,11 +14,30 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
-        console.log(position);
-        const {longitude} = position.coords;
-        const {latitude}  = position.coords;
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-        
+      console.log(position);
+      const { longitude } = position.coords;
+      const { latitude } = position.coords;
+      // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 20);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+      /*    mapboxgl.accessToken =
+        'pk.eyJ1IjoiY29naTI5OTYiLCJhIjoiY2xrdXhndGF2MG0zejNramtlcjBqbnY5eCJ9.gDjLb2gT-Ktjya6j-F7KIQ';
+      const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: [longitude, latitude], // starting position [lng, lat]
+        zoom: 20, // starting zoom
+      }); */
     },
     function () {
       alert('Could not get your position');
