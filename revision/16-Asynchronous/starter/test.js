60d567314557b2e6ec6ae429ebd14992e7b6fcb9
cogi2996 <bidang.tuyloan@gmail.com>
2023-10-27 00:17:35 +0700

    'add'

--- revision/16-Asynchronous/starter/test.js
+++ revision/16-Asynchronous/starter/test.js
@@ -20,17 +20,78 @@ const renderCountry = function (data, neighbour = '') {
     </div>
   </article>`;
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
 };
+// const getCountryDataAndNeighBour = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response => response.json())
+//     .then(([data]) => {
+//       renderCountry(data);
+//       return fetch(`https://restcountries.com/v3.1/alpha/${data.borders[0]}`);
+//     })
+//     .then(response => response.json())
+//     .then(([data]) => renderCountry(data, 'neighbour'));
+// };
+
+// getCountryDataAndNeighBour('portugal');
+
+// const renderError = function (msg) {
+//   countriesContainer.insertAdjacentText('beforeend', msg);
+// };
+
 const getCountryDataAndNeighBour = function (country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Something went wrong - ${response.status}  `);
+      return response.json();
+    })
     .then(([data]) => {
       renderCountry(data);
-      return fetch(`https://restcountries.com/v3.1/alpha/${data.borders[0]}`);
+      if (!data.borders[0]) throw new Error('Could not found neighbour');
+      const neighbour = data.borders[0];
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
     })
-    .then(response => response.json())
-    .then(([data]) => renderCountry(data, 'neighbour'));
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Could not found neighbour - ${response.status} `);
+      return response.json();
+    })
+    .then(([data, ...other]) => {
+      console.log(data);
+      renderCountry(data, 'neighbour');
+    })
+    .catch(err => {
+      console.log(`Something went wrong - ${err} `);
+      renderError(`Something went wrong ${err} `);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
 };
 
-getCountryDataAndNeighBour('portugal');
+// btn.addEventListener('click', function () {
+//   getCountryDataAndNeighBour('portugal');
+// });
+
+const whereAmI = function (lat, lng) {
+  fetch(
+    `https://geocode.xyz/${lat},${lng}?geoit=json&auth=121713347530917140919x56266`
+  )
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`can not get country - ${response.status} `);
+      return response.json();
+    })
+    .then(data => {
+      console.log(`You are in ${data.state},${data.country}`);
+      console.log(data);
+      return getCountryDataAndNeighBour(data.country);
+    })
+
+    .catch(err => {
+      console.log(`something went wrong - ${err}`);
+      alert(`something went wrong ${err}`);
+    });
+};
+
+whereAmI(52.508, 13.381);
