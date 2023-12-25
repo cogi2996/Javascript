488cb279b74e02da1d62b8e0d2afdb21a20e49a0
cogi2996 <bidang.tuyloan@gmail.com>
2023-12-25 10:44:59 +0700

    add

--- revision/16-Asynchronous/starter/script.js
+++ revision/16-Asynchronous/starter/script.js
@@ -444,13 +444,13 @@ const timeout = function (second) {
 //   Promise.resolve('success1'),
 // ])
 
-Promise.any([
-  Promise.resolve('success3'),
-  Promise.reject('ERROR'),
-  Promise.resolve('success2'),
-]).then(res => {
-  console.log(res);
-});
+// Promise.any([
+//   Promise.resolve('success3'),
+//   Promise.reject('ERROR'),
+//   Promise.resolve('success2'),
+// ]).then(res => {
+//   console.log(res);
+// });
 //   .then(res => console.log(res))
 //   .catch(err => console.error(err));
 
@@ -473,3 +473,51 @@ Promise.any([
 //     console.log(data);
 //   }
 // )();
+
+// coding challence #3
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+    img.addEventListener('load', function () {
+      imgContainer.appendChild(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+const loadNPause = async function () {
+  try {
+    let img = await createImage('img/img-1.jpg');
+    await wait(2);
+    img.style.display = 'none';
+    img = await createImage('img/img-2.jpg');
+    await wait(2);
+    img.style.display = 'none';
+  } catch (error) {
+    console.error(error);
+  }
+};
+// loadNPause();
+
+const loadAll = async function (imgArr) {
+  let imgs = imgArr.map(async function (img) {
+    return await createImage(img);
+  });
+  const imgsEl = Promise.all(imgs);
+  console.log(await imgsEl);
+};
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
